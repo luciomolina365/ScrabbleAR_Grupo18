3a8eaf687163e72cd79256fc72dd8d02e2b8e9e6
unknown <lucio_molina_365@live.com>
2020-07-21 02:35:46 -0300

    Cambios en CLASS

--- CLASS/TEST_Reloj_objeto.py
+++ CLASS/TEST_Reloj_objeto.py
@@ -8,12 +8,12 @@ layout = [  [sg.Text('Tiempo restante'), sg.T(' '*1), sg.Text(size=(8,1), key='-
 
 window = sg.Window('Reloj', layout, font='Default -24', return_keyboard_events=True)
 
-R = Reloj(0,5)      #Necesario 
-cantRead = 0        #Necesario 
+R = Reloj(0,5)                                  #Necesario 
+cantRead = 0                                    #Necesario 
 
 while not R.getTERMINO_Reloj() :                #R.getTERMINO_Reloj() Necesario             
     event, values = window.read(timeout=10)     #timeout=10 Necesario
-    cantRead = cantRead + 1  
+    cantRead = cantRead + 1                     #Necesario
 
     if event in (sg.WIN_CLOSED, 'Quit'):
         break
