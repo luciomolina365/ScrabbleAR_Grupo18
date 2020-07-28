f4e0c9ebc7ccabcc2dd3dfbce55c1680e3d6a773
Leandro Mikaliunas <mikaliunasleandro@gmail.com>
2020-07-28 20:41:42 -0300

    cambios 28-7

--- corroboro_y_puntuo/corroborarPalabra.py
+++ corroboro_y_puntuo/corroborarPalabra.py
@@ -1,87 +1,148 @@
 from pattern.text.es import parse, verbs, split, lexicon, spelling
+#from Objetos.CLASS_bolsa import Bolsa
 import puntuacion
 import posiciones_validas
 
 #Va a recibir un diccionario con este formato: {(x,y):'letra',...}
 #Corroboro al derecha y al reves, esta palabra que me manda el tablero
-        #Z 7.2 - O 7.3 - RR 7.7 - O 7.8
-dic2 = {(7, 5): 'O', (7, 3): 'O', (7, 2): 'Z', (7, 4): 'RR'}
+__palabra ={(4, 2):{"letra": 'RR',#9
+                "trampa": True,
+                "tipo_de_trampa": 1,
+                "recompensa": False,
+                "tipo_de_recompensa": None},
+        (4, 3):{"letra": "O", #6
+                "trampa": False,
+                "tipo_de_trampa": None,
+                "recompensa": True,
+                "tipo_de_recompensa": 3},
+        (4, 4):{"letra": "Z", #2
+                "trampa": False,
+                "tipo_de_trampa": None,
+                "recompensa": True,
+                "tipo_de_recompensa": 2},
+        (4, 1):{"letra": "O", #0
+                "trampa": True,
+                "tipo_de_trampa": 2,
+                "recompensa": False,
+                "tipo_de_recompensa": None}}
 
-def __informacion_de_turno(diccionario_que_recibe_del_tablero):
+#__configuracion = bolsa.getEstado()
+
+__configuracion = {'A':{'cantidad':11,'valor':1}, 'B':{'cantidad':3,'valor':1}, 'C':{'cantidad':4,'valor':1}, 'D':{'cantidad':4,'valor':1}, 
+'E':{'cantidad':11,'valor':2}, 'F':{'cantidad':2,'valor':1}, 'G':{'cantidad':2,'valor':1}, 'H':{'cantidad':2,'valor':1}, 'I':{'cantidad':6,'valor':1}, 
+'J':{'cantidad':2,'valor':1}, 'K':{'cantidad':1,'valor':1}, 'L':{'cantidad':4,'valor':1}, 'LL':{'cantidad':1,'valor':1}, 'M':{'cantidad':3,'valor':1}, 
+'N':{'cantidad':5,'valor':1}, 'Ñ':{'cantidad':1,'valor':1}, 'O':{'cantidad':8,'valor':3}, 'P':{'cantidad':2,'valor':2}, 'Q':{'cantidad':1,'valor':1}, 
+'R':{'cantidad':4,'valor':1}, 'RR':{'cantidad':1,'valor':10}, 'S':{'cantidad':7,'valor':1}, 'T':{'cantidad':4,'valor':1}, 'U':{'cantidad':6,'valor':1},
+'V':{'cantidad':2,'valor':1}, 'W':{'cantidad':1,'valor':1}, 'X':{'cantidad':1,'valor':1}, 'Y':{'cantidad':1,'valor':1}, 'Z':{'cantidad':1,'valor':5}}
+
+def __ordenar_info(diccionario_que_recibe_del_tablero):
     """ Recibo un diccionario enviado por el tablero, trabajado previamente para que llegue de el estilo que esta arriba,
     lo ordeno de menor a mayor y lo transformo a string para usarlo y corroborarlo con si es un sustantivo, adjetivo o verbo """
-    #Booleano para ver si la funcion privada me retorna que la palabra fue encontrada
-    
+        
     #Ordeno el diccionario por posiciones de menor a mayor
     haciaIzq=dict(sorted(diccionario_que_recibe_del_tablero.items(), key = lambda diccio: diccio[0]))
 
     #Ordeno el diccionario por posiciones de mayor a menor
     haciaDer=dict(sorted(diccionario_que_recibe_del_tablero.items(), key = lambda diccio: diccio[0],reverse=True))
+
+    #ME RETORNA EL DICCIONARIO QUE ENTRA POR LA PALABRA INGRESADA EN EL TABLERO, ORDENADA EN AMBOS SENTIDOS
+    #RETORNA DICCIONARIO ORDENADO
+    return(haciaIzq,haciaDer)
     
-    def __corroboro_palabra(diccionario_trabajado):
-        
-        palabra=""
-        palabra_lista=[]
-
-        #Con el for, me quedo con las letras que ingresaron
-        for k,v in diccionario_trabajado.items():
-            palabra = palabra + v
-            palabra_lista.append(v)
-
-                        
-        #Hago el parse para que me identifique la palabra
-        pal = palabra.lower()
-        pal = parse(pal).split()
-        
-        #Obtengo del parse la palabra para ir corroborando con el lexicon
-        pal_final = pal[0][0]
-
-        #Inicializo un booleano en false, esto es para saber si encontro o no la palabra
-        ok=False
-        for x in lexicon.keys():
-            if x in spelling.keys():
-                if (pal_final[0]==x):
-                    ok=True
-        
-        #Consulto si la palabra es sustantivo, adjetivo o verbo
-        for elemento in pal:
-            for elem in elemento:
-                if ((elem[1] == 'VB') or ((elem [1] == "NNS") or (elem[1] ==  "NN")) or (elem[1] == 'JJ')):
-                    return(ok,elem[0],palabra_lista)
+def __obtengo_diccionario_trabajado(__diccionario_ordenado):
+    #Agarro la info que me interesa para obtener la palabra y retornarla para verificarla despues, del diccionario que recibo, armo uno con las posiciones y la letra de la posicion
+    dic_trabajado = {}
+    for k,v in __diccionario_ordenado.items():
+        dic_trabajado[k] = v['letra']
     
-    #LLamo la funcion privada para obetener la informacion
-    def __retorno_puntuacion():    
-        if(__corroboro_palabra(haciaIzq)[0] == True):
-            pal_izq = __corroboro_palabra(haciaIzq)[2]
-            return(puntuacion.__puntuar_jugador(pal_izq))
-        
-        elif(__corroboro_palabra(haciaDer)[0] == True):
-            pal_der = __corroboro_palabra(haciaDer)[2]
-            return(puntuacion.__puntuar_jugador(pal_der))
+    #ME RETORNA UN DICCIONARIO DONDE LA CLAVE VA A SER LA POSICION Y EL VALOR LA LETRA, ME SIRVE PARA CORROBORAR POSICIONES Y PUNTUAR
+    #RETORNA DICCIONARIO TRABAJADO
+    return(dic_trabajado)
 
+
+def __corroboro_palabra(diccionario_trabajado):
     
-    if(__corroboro_palabra(haciaIzq)[0]==True):
-        if(posiciones_validas.__posiciones_validas(haciaIzq)==True):
-            info_final = __retorno_puntuacion()
+    palabra=""
+    palabra_lista=[]
+
+    #Con el for, me quedo con las letras que ingresaron
+    for k,v in diccionario_trabajado.items():
+        palabra = palabra + v
+        palabra_lista.append(v)
+
+                    
+    #Hago el parse para que me identifique la palabra
+    pal = palabra.lower()
+    pal = parse(pal).split()
+    
+    #Obtengo del parse la palabra para ir corroborando con el lexicon
+    pal_final = pal[0][0]
+
+    #Inicializo un booleano en false, esto es para saber si encontro o no la palabra
+    ok=False
+    for x in lexicon.keys():
+        if x in spelling.keys():
+            if (pal_final[0]==x):
+                ok=True
+    
+    #Consulto si la palabra es sustantivo, adjetivo o verbo
+    for elemento in pal:
+        for elem in elemento:
+            if ((elem[1] == 'VB') or ((elem [1] == "NNS") or (elem[1] ==  "NN")) or (elem[1] == 'JJ')):
+
+                #ME RETORNA UNA LISTA DONDE LA POS 0 RETORNA SI LA PALABRA QUE SE INGRESO SE ENCONTRO O NO EN PATTERN
+                #LA POS 1 ME RETORNA LA PALABRA EN SI
+                return(ok,elem[0])
+
+
+def __retorno_informacion(__palabra):    
+
+    #ORDENO EL DICCIONARIO QUE ENTRA DEL TABLERO pos 0 IZQUIERDA pos 1 DERECHA
+    palabra_ordenada = __ordenar_info(__palabra)
+    
+    palabra_ordenada_izq = palabra_ordenada[0] 
+    
+    palabra_ordenada_der = palabra_ordenada[1]
+    
+    #OBTENGO LA ESTRUCTURA QUE VOY A UTILIZAR PARA EMPEZAR A CORROBORAR TODA LA INFORMACION
+    palabra_izq = __obtengo_diccionario_trabajado(palabra_ordenada_izq)
+    
+    palabra_der = __obtengo_diccionario_trabajado(palabra_ordenada_der)
+       
+    if(__corroboro_palabra(palabra_izq)[0] == True):
+        
+        if(posiciones_validas.__posiciones_validas(palabra_izq)==True):
+            
+            info_final = puntuacion.__puntuar_jugador(palabra_ordenada_izq,__configuracion,palabra_izq)
             ok = True
         else:
+
             info_final = "TU PALABRA ES CORRECTA PERO, NO LA UBICASTE CORRECTAMENTE!!!"
             ok = False
-    elif(__corroboro_palabra(haciaDer)[0]==True):
-        if(posiciones_validas.__posiciones_validas(haciaDer)==True):
-            info_final = __retorno_puntuacion()
+    
+    elif(__corroboro_palabra(palabra_der)[0] == True):
+        
+        if(posiciones_validas.__posiciones_validas(palabra_der)==True):
+            
+            info_final = puntuacion.__puntuar_jugador(palabra_ordenada_der,__configuracion,palabra_der)
             ok = True
         else:
+
             info_final = "TU PALABRA ES CORRECTA PERO, NO LA UBICASTE CORRECTAMENTE!!!"
             ok = False
+    
     else:
+
         info_final = "INGRESASTE UNA PALABRA INCORRECTA, MEJOR SUERTE EN EL PROXIMO TURNO"
         ok=False
     
-    return(ok,info_final)
+    return(info_final)
 #-----------------------------------------------------------------------------------
 #TESTEOS, funcionan
-#palabra = __informacion_de_turno(dic2)
-#print(palabra)
+print("puntuacion de la palabra ingresada")
+print(__retorno_informacion(__palabra))
+
+
+
 
 #print(corroboro_palabra.__doc__)
--- corroboro_y_puntuo/puntuacion.py
+++ corroboro_y_puntuo/puntuacion.py
@@ -1,62 +1,54 @@
+#Estructuras que tienen que entrar aca
+
 #Estructura que obtengo de la configuracion del juego
-__configuracion = {'A':{'cantidad':11,'valor':1}, 'B':{'cantidad':3,'valor':1}, 'C':{'cantidad':4,'valor':1}, 'D':{'cantidad':4,'valor':1}, 
-'E':{'cantidad':11,'valor':2}, 'F':{'cantidad':2,'valor':1}, 'G':{'cantidad':2,'valor':1}, 'H':{'cantidad':2,'valor':1}, 'I':{'cantidad':6,'valor':1}, 
-'J':{'cantidad':2,'valor':1}, 'K':{'cantidad':1,'valor':1}, 'L':{'cantidad':4,'valor':1}, 'LL':{'cantidad':1,'valor':1}, 'M':{'cantidad':3,'valor':1}, 
-'N':{'cantidad':5,'valor':1}, 'Ñ':{'cantidad':1,'valor':1}, 'O':{'cantidad':8,'valor':2}, 'P':{'cantidad':2,'valor':2}, 'Q':{'cantidad':1,'valor':1}, 
-'R':{'cantidad':4,'valor':1}, 'RR':{'cantidad':1,'valor':8}, 'S':{'cantidad':7,'valor':1}, 'T':{'cantidad':4,'valor':1}, 'U':{'cantidad':6,'valor':1},
-'V':{'cantidad':2,'valor':1}, 'W':{'cantidad':1,'valor':1}, 'X':{'cantidad':1,'valor':1}, 'Y':{'cantidad':1,'valor':1}, 'Z':{'cantidad':1,'valor':2}}
+#__configuracion = {'A':{'cantidad':11,'valor':1}, 'B':{'cantidad':3,'valor':1}, 'C':{'cantidad':4,'valor':1}, 'D':{'cantidad':4,'valor':1}, 
+#'E':{'cantidad':11,'valor':2}, 'F':{'cantidad':2,'valor':1}, 'G':{'cantidad':2,'valor':1}, 'H':{'cantidad':2,'valor':1}, 'I':{'cantidad':6,'valor':1}, 
+#'J':{'cantidad':2,'valor':1}, 'K':{'cantidad':1,'valor':1}, 'L':{'cantidad':4,'valor':1}, 'LL':{'cantidad':1,'valor':1}, 'M':{'cantidad':3,'valor':1}, 
+#'N':{'cantidad':5,'valor':1}, 'Ñ':{'cantidad':1,'valor':1}, 'O':{'cantidad':8,'valor':3}, 'P':{'cantidad':2,'valor':2}, 'Q':{'cantidad':1,'valor':1}, 
+#'R':{'cantidad':4,'valor':1}, 'RR':{'cantidad':1,'valor':10}, 'S':{'cantidad':7,'valor':1}, 'T':{'cantidad':4,'valor':1}, 'U':{'cantidad':6,'valor':1},
+#'V':{'cantidad':2,'valor':1}, 'W':{'cantidad':1,'valor':1}, 'X':{'cantidad':1,'valor':1}, 'Y':{'cantidad':1,'valor':1}, 'Z':{'cantidad':1,'valor':5}}
 
+#__palabra ={(7, 2): {'letra': 'Z', 'trampa': False, 'tipo_de_trampa': None, 'recompensa': True, 'tipo_de_recompensa': 2}, #4
+#            (7, 3): {'letra': 'O', 'trampa': True, 'tipo_de_trampa': 2, 'recompensa': False, 'tipo_de_recompensa': None}, #0
+#            (7, 4): {'letra': 'RR', 'trampa': True, 'tipo_de_trampa': 1, 'recompensa': False, 'tipo_de_recompensa': None},#7
+#            (7, 5): {'letra': 'O', 'trampa': False, 'tipo_de_trampa': None, 'recompensa': True, 'tipo_de_recompensa': 3}} #6
 
-#Estructura que obtengo de la interfaz del tablero
-__tablero ={(7, 4):{"letra": 'RR',#9
-                "trampa": True,
-                "tipo_de_trampa": 1,
-                "recompensa": False,
-                "tipo_de_recompensa": None},
-        (7, 5):{"letra": "O", #6
-                "trampa": False,
-                "tipo_de_trampa": None,
-                "recompensa": True,
-                "tipo_de_recompensa": 3},
-        (7, 2):{"letra": "Z", #2
-                "trampa": False,
-                "tipo_de_trampa": None,
-                "recompensa": True,
-                "tipo_de_recompensa": 2},
-        (7, 3):{"letra": "O", #0
-                "trampa": True,
-                "tipo_de_trampa": 2,
-                "recompensa": False,
-                "tipo_de_recompensa": None}}
+#Estructura que obtengo en base a lo que entra en corroboro palabra
+#Y va a ingresar a puntuacion, si y solo si, es correcta la palabra
 
-def __armo_mi_tablero(__tablero):
-    #Obtengo el tablero como quiero, para trabajar con el
-    __tablero2={}
-    for k,v in __tablero.items():
-        __tablero2[v['letra']]=v
-    for k,v in __tablero2.items():
-        del __tablero2[k]['letra']
-
-    return(__tablero2)
-
-def __puntuar_jugador(__palabra):
-    
-    __tablero2=__armo_mi_tablero(__tablero)
+#__palabra_lista = {(7, 2): 'Z', (7, 3): 'O', (7, 4): 'RR', (7, 5): 'O'}
+# Z = 5 - O = 3 - RR = 10
+# Z = 10 - O = 1 - RR = 9 - O = 9
+# 10 +10 +9 = 29
+def __puntuar_jugador(__palabra,__configuracion,__palabra_lista):
     
     puntuacionActual = 0
-    
-    for i in range(0,(len(__palabra))):
-        letra = __palabra[i]
-        valor = __configuracion[letra]['valor']
-        trampa = __tablero2[letra]['trampa']
-        valor_trampa = __tablero2[letra]['tipo_de_trampa']
-        bonus = __tablero2[letra]['recompensa']
-        valor_bonus = __tablero2[letra]['tipo_de_recompensa']
+    puntuacion_letra = 0
+    for coor in __palabra_lista:
+        
+        valor = __configuracion[__palabra[coor]['letra']]['valor']
+        
+        trampa = __palabra[coor]['trampa']
+        
+        bonus = __palabra[coor]['recompensa']
+        
         if(trampa == True):
-            puntuacionActual = puntuacionActual + (valor - valor_trampa)
+            valor_trampa = __palabra[coor]['tipo_de_trampa']
+
+            puntuacion_letra = valor - valor_trampa
+
         elif(bonus == True):
-            puntuacionActual = puntuacionActual + (valor * valor_bonus)
+            valor_bonus = __palabra[coor]['tipo_de_recompensa']
+
+            puntuacion_letra = valor * valor_bonus
+
         else:
-            puntuacionActual = puntuacionActual + valor
-    
+
+            puntuacion_letra = valor
+                
+        puntuacionActual = puntuacionActual + puntuacion_letra
+
     return (puntuacionActual)
+
+#Testeado con las estructuras que estan arriba, ya esta funcionando
+#print(__puntuar_jugador(__palabra,__configuracion,__palabra_lista))
