18ea46ac8983ad1fa10ce82fcca5a90cba20f4a8
unknown <lucio_molina_365@live.com>
2020-08-06 08:56:11 -0300

    ¿ANDA!

--- ScrabbleAR.py
+++ ScrabbleAR.py
@@ -14,7 +14,7 @@ def Menu_principal():
             [sg.Button("Confirmar",key="confirmar"),sg.Cancel(button_color=('black','white'))]]
 
         window = sg.Window('Partidas guardadas', layout,background_color="black")
-        datos=None
+        datos = None
         while True:
             event, values= window.read()
             if event=="Cancel":
@@ -33,35 +33,35 @@ def Menu_principal():
         
         """Muestra una nueva ventana con una listbox de top ten dependiendo la dificultad que elijas.  Del fomato nombre, puntaje , dificultad y fecha."""
 
-        layout=[[sg.Text('Seleccione la dificultad para ver el Top Ten',text_color="white",background_color="black")],
+        layout = [[sg.Text('Seleccione la dificultad para ver el Top Ten',text_color="white",background_color="black")],
             [sg.Button("Facil",key="facil",button_color=('black','white')),sg.Button("Medio",key="medio",button_color=('black','white')),sg.Button("Dificil",key="dificil",button_color=('black','white'))],
             [sg.Listbox([], size = (70,15) , key = "listBox" , select_mode=False)],
             [sg.Cancel(button_color=('black','white'))]]
 
         window = sg.Window('Top Ten', layout,background_color="black")
-        No_hay_partidas=["No hay registros en esta dificultad"]
+        No_hay_partidas = ["No hay registros en esta dificultad"]
 
         while True:
-            event, values= window.read()
+            event, values = window.read()
             
-            if event=="Cancel":
+            if event == "Cancel":
                 window.close()
                 break
             
-            if event=="dificil":
-                if topDificil==[]:
+            if event == "dificil":
+                if topDificil == []:
                     window["listBox"].update(No_hay_partidas)
                 else:
                     window["listBox"].update(topDificil)
             
-            if event=="medio":
-                if topMedio==[]:
+            if event == "medio":
+                if topMedio == []:
                     window["listBox"].update(No_hay_partidas)
                 else:
                     window["listBox"].update(topMedio)    
             
-            if event=="facil":
-                if topFacil==[]:
+            if event == "facil":
+                if topFacil == []:
                     window["listBox"].update(No_hay_partidas)
                 else:
                     window["listBox"].update(topFacil)
@@ -78,16 +78,16 @@ def Menu_principal():
     Iniciar = [sg.Button("Iniciar Partida",size=(10,5),key="_iniciar_",button_color=('white','grey'))]
 
     if(ok == False):
-        Cargar=[sg.Button("Cargar Partida",size=(10,5),disabled=True, key="cargar",button_color=('white','grey'))]
+        Cargar = [sg.Button("Cargar Partida",size=(10,5),disabled=True, key="cargar",button_color=('white','grey'))]
     else:
-        Cargar=[sg.Button("Cargar Partida",size=(10,5),enable_events=True, key="cargar",button_color=('white','grey'))]
+        Cargar = [sg.Button("Cargar Partida",size=(10,5),enable_events=True, key="cargar",button_color=('white','grey'))]
 
     titulo =  [[sg.Text("Scrabble", size=(22,10),auto_size_text=True)]]
 
     Top=[[sg.Button(("Top Ten"),key="topTen",size=(22,1))]] 
 
 
-    layout= titulo + [Iniciar + Cargar ] + Top
+    layout = titulo + [Iniciar + Cargar ] + Top
 
     window = sg.Window('ScrabbleAr', layout, font='Courier 12',background_color="black")
 
@@ -95,12 +95,12 @@ def Menu_principal():
 
 
     while True:
-        event, values= window.read()
+        event, values = window.read()
 
         if event == "cargar":
-            lista=archivos.lista_de_partidas_a_cargar()
-            direccion=mostrar_partidas_guardadas(lista)
-            if direccion!= None:
+            lista = archivos.lista_de_partidas_a_cargar()
+            direccion = mostrar_partidas_guardadas(lista)
+            if direccion != None:
                 partida = archivos.cargarPartida(direccion)
                 window.close()
                 Tablero.juego(partida)
@@ -111,10 +111,10 @@ def Menu_principal():
             jugar()
             break
 
-        if(event=="topTen"):
-            topFacil=archivos.TopTen_de_jugadores(1)
-            topMedio=archivos.TopTen_de_jugadores(2)
-            topDificil=archivos.TopTen_de_jugadores(3)
+        if(event == "topTen"):
+            topFacil = archivos.TopTen_de_jugadores(1)
+            topMedio = archivos.TopTen_de_jugadores(2)
+            topDificil = archivos.TopTen_de_jugadores(3)
             mostrar_ten(topFacil,topMedio,topDificil)
 
 if __name__ == "__main__":
